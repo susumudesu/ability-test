4278f0b90205709cab36187997e59d74c2970055
susumu <susumudesuyo@gmail.com>
2024-09-13 21:40:55 +0900

    確認テスト

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+
+<div class="contact-form__content">
+      <div class="contact-form__heading">
+        <h2>Contact</h2>
+      </div>
+      <form class="form">
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お名前</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="text" name="name" placeholder="例: 田中" /><input type="text" name="name" placeholder="例: 太郎" />
+              
+            </div>
+            <div class="form__error">
+              
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">メールアドレス</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="email" name="email" placeholder="例: test@example.com" />
+            </div>
+            <div class="form__error">
+              
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">電話番号</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="tel" name="tel" placeholder="09012345678" />
+            </div>
+            <div class="form__error">
+              
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">住所</span>
+            <span class="form__label--required">※</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="text" name="name" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3" />
+            </div>
+            <div class="form__error">
+              
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">建物名</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="text" name="name" placeholder="テスト太郎" />
+            </div>
+            <div class="form__error">
+              
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お問い合わせ内容</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--textarea">
+              <textarea name="content" placeholder="お問い合わせ内容をご記載ください"></textarea>
+            </div>
+          </div>
+        </div>
+        <div class="form__button">
+          <button class="form__button-submit" type="submit">確認画面</button>
+        </div>
+      </form>
+    </div>
+
+    @endsection
